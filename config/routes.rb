aa0dc70926b704bb47e59424f8f0d92984770a0c
keikosmile <69710419+keikosmile@users.noreply.github.com>
2021-10-14 21:42:35 +0900

    フォームの送信先を指定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'posts/index', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
   get 'posts/:id', to: 'posts#show'
+  post 'posts/create', to: 'posts#create'
 
   get '/', to: 'home#top'
   get 'about', to: 'home#about'
