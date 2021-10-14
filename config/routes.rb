9323dcacf672aa033df54707e470d7d4a970329d
keikosmile <69710419+keikosmile@users.noreply.github.com>
2021-10-14 13:40:03 +0900

    postsコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get 'posts/index', to: 'posts#index'
+
   get '/', to: 'home#top'
   get 'about', to: 'home#about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
